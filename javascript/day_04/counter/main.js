40d28b132c4c7fcd7d2d824524b5ad8fdb7ac1d4
NieT29 <tienlevan3010@gmail.com>
2024-03-27 15:34:39 +0700

    up-homework-4

--- javascript/day_04/counter/main.js
+++ javascript/day_04/counter/main.js
@@ -22,33 +22,5 @@ nextBtn.addEventListener("click", () => {
     update();
 }) 
 
-// let totalBox = 0;
 
-// function updateBox() {
-//     const points = document.querySelector(".points")
-//     points.innerText = totalBox;
-// }
-
-// function renderBox() {
-//     for (let i = 0; i < colors.length; i++) {
-//         const box = document.createElement("div")
-//         totalBox++
-//         updateBox(totalBox)
-//         box.classList.add("box")
-//         box.style.backgroundColor = colors[i]
-//         box.addEventListener('click', () => {
-//             box.remove();
-//             totalBox--;
-//             updateBox(totalBox);
-//         });
-//         document.body.appendChild(box)
-//     }
-// }
-
-// renderBox();
-
-// const btn = document.querySelector("#btn")
-// btn.addEventListener("click", ()=> {
-//     renderBox();
-// })
 
